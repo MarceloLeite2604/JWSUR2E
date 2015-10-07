1bf3d5a9e66b819b6c38448d696ed07e65591208
Marcelo Leite <marceloleite2604@gmail.com>
2015-10-07 17:20:46 -0300

    The 17th example from the fourth chapter.

---
+++ Chapter 4 - Example 17/src/main/java/br/com/acaosistemas/randasyncclient/RandClientAsync.java
@@ -0,0 +1,33 @@
+package br.com.acaosistemas.randasyncclient;
+
+import java.util.List;
+
+import javax.xml.ws.AsyncHandler;
+import javax.xml.ws.Response;
+
+public class RandClientAsync {
+	public static void main(String[] args) {
+		RandServiceService service = new RandServiceService();
+		RandService port = service.getRandServicePort();
+		port.nextNAsync(4, new MyHandler());
+		try {
+			Thread.sleep(5000); // in production, do something useful!
+		} catch (Exception e) {
+		}
+		System.out.println("\nmain is exitting...");
+	}
+
+	static class MyHandler implements AsyncHandler<NextNResponse> {
+		public void handleResponse(Response<NextNResponse> future) {
+			try {
+				NextNResponse response = future.get();
+				List<Integer> nums = response.getReturn();
+				for (Integer num : nums)
+					System.out.println(num);
+			} catch (Exception e) {
+				System.err.println(e);
+			}
+		}
+	}
+
+}
